b51fe12ff997dd77e3922ecf7178faccb944adac
Zulfa Iflakhul Fadilla <89766936+zulfaiflakhul@users.noreply.github.com>
2022-09-15 00:56:42 +0700

    update layout

--- application/views/layout/home.php
+++ application/views/layout/home.php
@@ -13,7 +13,7 @@
                         </a>
                     </div>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-lg-6 text-center align-self-center">
                     <div class="">
                     <img src="<?php echo base_url()?>assets/img/bg-umkm-1.png" alt=""/>
                     </div>
