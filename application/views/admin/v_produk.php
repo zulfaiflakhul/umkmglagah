bd707e20588995692279e27da19a4b0e636f1ac4
Zulfa Iflakhul Fadilla <89766936+zulfaiflakhul@users.noreply.github.com>
2022-09-15 10:40:23 +0700

    edit table

--- application/views/admin/v_produk.php
+++ application/views/admin/v_produk.php
@@ -74,7 +74,7 @@
                  </div>
                  <!-- /.box-header -->
                  <div class="box-body">
-                   <table id="example1" class="table-responsive-md" style="font-size:13px;">
+                   <table id="example1" class="table table-striped" style="font-size:13px;">
                      <thead>
                        <tr>
                          <th>Gambar</th>
