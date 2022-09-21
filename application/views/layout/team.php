f8b261264f91cb2b4b5d231803087ef097e5d18a
Zulfa Iflakhul Fadilla <89766936+zulfaiflakhul@users.noreply.github.com>
2022-09-21 10:17:01 +0700

    update team

--- application/views/layout/team.php
+++ application/views/layout/team.php
@@ -4,48 +4,7 @@
                 <div class="text-center">
                     <h2 class="section-heading text-uppercase">desa glagah</h2>
                 </div>
-                <!--<div class="row">
-                    <div class="col-lg-3">
-                        <div class="team-member">
-                            <img class="mx-auto rounded-circle" src="<?php echo base_url()?>assets/img/team/123novi.jpg" alt="" />
-                            <h4>Novi Alfiani</h4>
-                            <p class="text-muted">181240000850</p>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-instagram"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="team-member">
-                            <img class="mx-auto rounded-circle" src="<?php echo base_url()?>assets/img/team/123ira.jpeg" alt="" />
-                            <h4>Ira Nala Khayaroh</h4>
-                            <p class="text-muted">181240000864</p>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-instagram"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="team-member">
-                            <img class="mx-auto rounded-circle" src="<?php echo base_url()?>assets/img/team/123anabel.jpg" alt="" />
-                            <h4>Noviana Bella H</h4>
-                            <p class="text-muted">181240000815</p>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-instagram"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="team-member">
-                            <img class="mx-auto rounded-circle" src="<?php echo base_url()?>assets/img/team/123nanda.jpg" alt="" />
-                            <h4>Sani Nanda Rafika</h4>
-                            <p class="text-muted">18124000800</p>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-instagram"></i></a>
-                        </div>
-                    </div>
-                </div>-->
+            
                 <div class="row">
                     <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Desa Glagah terletak di sebelah barat desa Cawan yang sebagian penduduknya berprofesi sebagai pedagang buah dan sayuran, atau biasa orang menyebut sebagai "bakul penebas". Di desa Glagah terdapat Sendang (kolam air) untuk pemandian dan pengairan sawah disekitarnya. Konon sendang Glagah ini dibuat oleh seorang pawang ular sakti. Menurut Ki Awal Kulonkaman (sesepuh desa Glagah) yang sampai saat ini masih disegani oleh warga masyarakat sekitar, dia mengatakan bahwa sendang glagah di buat pada tahun 1411 oleh Mbah Blendang.</p></div>
                 </div>
@@ -59,16 +18,16 @@
         <div class="py-5">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-3 col-sm-6 my-3">
+                    <div class="col-md-3 col-sm-6">
                         <a href="#!"><img class="img-fluid d-block mx-auto" src="<?php echo base_url()?>assets/img/logos/logo-klaten.png" alt="" /></a>
                     </div>
-                    <div class="col-md-3 col-sm-6 my-3">
+                    <div class="col-md-3 col-sm-6">
+                        <a href="#!"><img class="img-fluid d-block mx-auto" src="<?php echo base_url()?>assets/img/logos/logo-uny.png" alt="" /></a>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
                         <a href="#!"><img class="img-fluid d-block mx-auto" src="<?php echo base_url()?>assets/img/logos/logo-dilan-dark.png" alt="" /></a>
                     </div>
-                    <div class="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img class="img-fluid d-block mx-auto" src="<?php echo base_url()?>assets/img/logos/logo-kkn.png" alt="" /></a>
-                    </div>
-                    <div class="col-md-3 col-sm-6 my-3">
+                    <div class="col-md-3 col-sm-6">
                         <a href="#!"><img class="img-fluid d-block mx-auto" src="<?php echo base_url()?>assets/img/logos/hut-klaten.png" alt="" /></a>
                     </div>
                 </div>
